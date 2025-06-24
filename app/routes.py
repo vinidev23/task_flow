369b1ae8f3478adc893f8a4cff92323d0d2372a9
vinidev23 <vinidev23>
2025-06-24 13:59:43 -0300

    Adicionando as rotas para o painel de tarefas

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,16 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from flask_login import login_required, current_user
+from .models import Task
+from . import db
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def home():
     return render_template('home.html')
+
+@main.route('/dashboard')
+@login_required
+def dashboard():
+    tarefas = Task.query.filter_by(user_id = current_user.id).order_by(Task.due_date).all()
+    return render_template('dashboard.html', tarefas = tarefas)
