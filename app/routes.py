a246c4aa421945260711db384c1cc938198617fe
vinidev23 <vinidev23>
2025-06-24 20:17:34 -0300

    Adicionando a função de editar as tarefas

--- app/routes.py
+++ app/routes.py
@@ -48,3 +48,33 @@ def nova_tarefa():
         return redirect(url_for('main.dashboard'))
 
     return render_template('nova_tarefa.html')
+
+@main.route('/editar/<int:id>', methods=['GET', 'POST'])
+@login_required
+def editar_tarefa(id):
+    tarefa = Task.query.get_or_404(id)
+    
+    if tarefa.user_id != current_user.id:
+        flash("Você não tem permissão para editar esta tarefa.")
+        return redirect(url_for('main.dashboard'))
+    
+    if request.method == 'POST':
+        tarefa.title = request.form['title']
+        tarefa.description = request.form['description']
+        tarefa.status = request.form['status']
+        data_entrega = request.form['due_date']
+        
+        if data_entrega:
+            try:
+                tarefa.due_date = datetime.strptime(data_entrega, '%Y-%m-%d').date()
+            except ValueError:
+                flash('Data inválida')
+                return redirect(url_for('main.editar_tarefa', id = id))
+        else:
+            tarefa.due_date = None
+            
+        db.session.commit()
+        flash('Tarefa editada com sucesso!')
+        return redirect(url_for('main.dashboard'))
+    
+    return render_template('editar_tarefa.html', tarefa = tarefa)
