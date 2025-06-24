144ca60b17532443fe2cfbbf341d8bed8abe96c2
vinidev23 <vinidev23>
2025-06-23 21:18:19 -0300

    Inciando arquivo, rotas e configurações do banco de dados

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,32 @@
 from flask import Flask
-from .routes import main
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = '123456'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../instance/apptarefas.sqlite'
+    
+    db.init_app(app)
+    login_manager.init_app(app)
+    
+    from .models import User
+    from .routes import main
+    from .auth import auth
+    
     app.register_blueprint(main)
+    app.register_blueprint(auth)
+    
+    with app.app_context():
+        db.create_all()
+        
     return app
+
+@login_manager.user_loader
+def load_user(user_id):
+    from .models import User
+    return User.query.get(int(user_id))
---
+++ app/auth.py
@@ -0,0 +1,51 @@
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from .models import User
+from . import db
+from flask_login import login_user, logout_user, login_required
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        print("Recebido com Sucesso")
+        
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        
+        if user and user.check_password(password):
+            login_user(user)
+            return redirect(url_for('main.home'))
+        else:
+            flash('Usuário ou senha inválidos')
+        return render_template('login.html')
+    
+    return render_template('login.html')
+    
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user:
+            flash('Nome de usuário já existe')
+            return redirect(url_for('auth.register'))
+        
+        new_user = User(username=username)
+        new_user.set_password(password)
+        
+        db.session.add(new_user)
+        db.session.commit()
+        flash('Cadastro realizado com sucesso! Faça Login')
+        return redirect(url_for('auth.login'))
+    
+    return render_template('register.html')
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('main.home'))
