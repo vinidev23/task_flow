b42a2dbe29d0388242251bc82b88032ef36c524e
vinidev23 <vinidev23>
2025-06-24 15:08:58 -0300

    Adicionando função de adicionar tarefa, descrição, o status e a data de entrega no painel

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_required, current_user
 from .models import Task
 from . import db
+from datetime import datetime
 
 main = Blueprint('main', __name__)
 
@@ -14,3 +15,36 @@ def home():
 def dashboard():
     tarefas = Task.query.filter_by(user_id = current_user.id).order_by(Task.due_date).all()
     return render_template('dashboard.html', tarefas = tarefas)
+
+@main.route('/nova', methods=['GET', 'POST'])
+@login_required
+def nova_tarefa():
+    if request.method == 'POST':
+        titulo = request.form['title']
+        descricao = request.form['description']
+        status = request.form['status']
+        data_entrega = request.form['due_date']
+        
+        if data_entrega:
+            try:
+                data_entrega = datetime.strptime(data_entrega, '%Y-%m-%d').date()
+            except ValueError:
+                flash('Data inválida')
+                return redirect(url_for('main.nova_tarefa'))
+        else:
+            data_entrega = None
+        
+        nova = Task(
+            title=titulo,
+            description=descricao,
+            status=status,
+            due_date=data_entrega,
+            user_id=current_user.id
+        )
+        
+        db.session.add(nova)
+        db.session.commit()
+        flash('Tarefa criada com sucesso!')
+        return redirect(url_for('main.dashboard'))
+
+    return render_template('nova_tarefa.html')
